ec52ff63c31c7b9d9fcc7016481811444ac7004f
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-21 20:03:11 -0400

    added methods github class

--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -3,6 +3,25 @@ module Myhub
     include HTTParty
     base_uri "https://api.github.com"
 
-    # Your code here too!
+    def initialize
+      @headers = {
+        "Authorization" => "token #{ENV["AUTH_TOKEN"]}", 
+        "User-Agent" => "HTTParty"
+      }
+    end
+
+    def get_issues(owner, repo, options={})    # GET /repos/:owner/:repo/issues
+    issues = Github.get("/repos/#{owner}/#{repo}/issues", :headers => @headers, :body options)
+    end
+    
+    def reopen_issue(owner, repo, id) #need ID        #PATCH /repos/:owner/:repo/issues/:number
+    Github.patch("/repos/#{owner}/#{repo}/issues/#{number}", :headers => @headers, :body => {state: "open"}.to_json)
+    end
+
+    def close_issue(owner, repo, id)   #need ID         #PATCH /repos/:owner/:repo/issues/:number
+    Github.patch("/repos/#{owner}/#{repo}/issues/#{id}", :headers => @headers, :body => {state: "closed"}.to_json)
+    end
+     
   end
 end
+   
