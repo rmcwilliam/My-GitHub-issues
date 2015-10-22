ae9bbc516ed7ded6989a75884025d649c2e17521
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-21 22:13:25 -0400

    progress, not finished

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -5,12 +5,33 @@ require "pry"
 require "myhub/version"
 require "myhub/github"
 
-module Myhub
+
+module Myhub                  # Server / controller 
   class App < Sinatra::Base
     set :logging, true
 
-    # Your code here ...
+binding.pry
+    get "/" do
+      api = Github.new
+      results = api.get_issues("TIY-ATL-ROR-2015-Sep","assignments",{mentioned:"rmcwilliam", state: "all"})
+      erb :index, locals: {issues: results}
+    end
+
+    post "issue/reopen/:id" do    # owner, repo
+      api = Github.new
+      api.reopen_issue("TIY-ATL-ROR-2015-Sep","assignments",params["id"].to_i)
+      "It's open"
+    end
+
+    post "issue/close/:id" do      # owner, repo
+      api = Github.new
+      api.close_issue("TIY-ATL-ROR-2015-Sep","assignments",params["id"].to_i)
+      "It's closed"
+    end
 
     run! if app_file == $0
   end
+
 end
+
+# when I start the application do ->  AUTH_TOKEN=your_auth_token_here bundle exec ruby lib/myhub.rb     Sets auth token ENV
--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -13,6 +13,7 @@ module Myhub
     def get_issues(owner, repo, options={})    # GET /repos/:owner/:repo/issues
     issues = Github.get("/repos/#{owner}/#{repo}/issues", :headers => @headers, :body options)
     end
+        # Enumerate each, map... need to drill down and hand back options 
     
     def reopen_issue(owner, repo, id) #need ID        #PATCH /repos/:owner/:repo/issues/:number
     Github.patch("/repos/#{owner}/#{repo}/issues/#{number}", :headers => @headers, :body => {state: "open"}.to_json)
